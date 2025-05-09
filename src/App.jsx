5503b3bb6aadd39c7edd2fa50bc86e4d88823303
a7med-Mubarak <a7med22mubarak@gmail.com>
2025-05-10 01:05:55 +0300

    Changed browser router to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,7 @@
-// import { useState } from 'react'
-// import './App.css'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import Home from './modules/Home/Components/Home/Home'
-// import Analysis from './modules/Match analysis/Components/Match analysis/Analysis'
-// import NotFound from './modules/Shared/Components/NotFound/NotFound'
-// import MatchDay from './modules/MatchDay/Components/MatchDay'
-// import ContactUs from './modules/Contact Us/Components/ContactUs'
-// import AboutUs from './modules/AboutUs/Components/AboutUs'
-
-// function App() {
-
-
-//   const routes = createBrowserRouter([
-//     {
-//       errorElement:<NotFound/>,
-//       children:[
-//         {index:true,element:<Home/>},
-//         {path:'home',element:<Home/>},
-//         {path:'Analysis',element:<Analysis/>},
-//         {path:'MatchDay',element:<MatchDay/>},
-//         {path:'ContactUs',element:<ContactUs/>}, 
-//         {path:'AboutUs',element:<AboutUs/>}, 
-//       ]
-//     }
-//   ])
-
-
-
-//   return <RouterProvider router={routes} />
-// }
-
-// export default App
-
-
-
-
-
-
-
-
 
 import { useState } from 'react'
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
 import Home from './modules/Home/Components/Home/Home'
 import Analysis from './modules/Match analysis/Components/Match analysis/Analysis'
 import NotFound from './modules/Shared/Components/NotFound/NotFound'
@@ -52,7 +11,7 @@ import AboutUs from './modules/AboutUs/Components/AboutUs'
 
 function App() {
 
-  const routes = createBrowserRouter(
+  const routes = createHashRouter(
     [
       {
         errorElement: <NotFound />,
